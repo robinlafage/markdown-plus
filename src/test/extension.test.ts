cd029079ac432df814f9e3f6108f7c78b474c46a
robinlafage <robinlafage42@gmail.com>
2025-10-08 14:23:20 +0200

    test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,5 +1,7 @@
 import * as assert from 'assert';
 
+//Test
+
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
